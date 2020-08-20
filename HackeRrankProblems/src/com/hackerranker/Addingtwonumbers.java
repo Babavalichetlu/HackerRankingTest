0d002a9e973b995cb690e3c0b292943679024a4d
Babavali Ahmadi <babavali.chetlu@gmail.com>
2020-08-20 22:41:32 +0530

    Update Addingtwonumbers.java

--- HackeRrankProblems/src/com/hackerranker/Addingtwonumbers.java
+++ HackeRrankProblems/src/com/hackerranker/Addingtwonumbers.java
@@ -1,6 +1,4 @@
 #include <assert.h>
-Adding two number s using c language
-====================================
 #include <ctype.h>
 #include <limits.h>
 #include <math.h>
@@ -18,6 +16,7 @@ char* rtrim(char*);
 float parse_float(char*);
 
 
+
 /*
  * Complete the 'addNumbers' function below.
  *
@@ -33,4 +32,119 @@ int addNumbers(float a, float b) {
    return (num3);
 
 }
+
 int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    float a = parse_float(ltrim(rtrim(readline())));
+
+    float b = parse_float(ltrim(rtrim(readline())));
+
+    int result = addNumbers(a, b);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+float parse_float(char* str) {
+    char* endptr;
+    float value = strtof(str, &endptr);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
